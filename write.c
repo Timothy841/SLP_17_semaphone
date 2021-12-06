facd79913f9962c4513138fc25ebe995019f78c4
Timothy <tsit20@stuy.edu>
2021-12-06 11:09:55 -0500

    fixed

--- write.c
+++ write.c
@@ -32,7 +32,7 @@ int main(){
 	if (info.st_size > 0){
 		lseek(file, *line * -1, SEEK_END);
 		read(file, c, *line);
-		printf("Current line: %s\n", c);
+		printf("Current line: %s", c);
 	}
 	else{
 		printf("New story: \n");
@@ -41,6 +41,7 @@ int main(){
 	file = open("story.txt", O_WRONLY | O_APPEND);
 	fgets(c, 100, stdin);
 	write(file, c, strlen(c));
+	*line = strlen(c);
 	close(file);
 	shmdt(line);
 	sb.sem_op = 1; //set the operation to up
